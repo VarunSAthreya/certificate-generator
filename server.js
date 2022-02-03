514a6809205ad5210773bffb4969ee47f54f2e07
Varun S Athreya <varun.sathreya@gmail.com>
2022-02-03 17:01:33 +0530

    Minor style changes

--- server.js
+++ server.js
@@ -12,36 +12,48 @@ const app = express();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get("/", (req, res) => {
+    // server static html file
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", urlencodedParser, async (req, res) => {
     console.log("body:", req.body);
+    // get data form request body
     const data = req.body;
+
+    // get template html file
     const templateHtml = fs.readFileSync(
         path.resolve(__dirname, "./template/certificate.html"),
         "utf8"
     );
 
+    // compile template
     const template = handlebars.compile(templateHtml);
 
+    // Create buffer of the background image
     const bitmap = fs.readFileSync("./template/certificate.jpg");
     const base64image = new Buffer.from(bitmap).toString("base64");
 
+    // Add remaining data
     data.src = "data:image/png;base64," + base64image;
     data.date = format(new Date(data.date), "MMM. yyyy");
     data.id = uuidv4();
     data.ref = Math.floor(1000 + Math.random() * 9000);
 
+    // Add data to template
     const html = template(data);
 
+    // Launch puppeteer
     const browser = await puppeteer.launch({
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
+
     const page = await browser.newPage();
+
     // We set the page content as the generated html by handlebars
     await page.setContent(html);
+
     // We use pdf function to generate the pdf in the same folder as this file.
     await page.pdf({
         path: "certificate.pdf",
@@ -53,6 +65,8 @@ app.post("/", urlencodedParser, async (req, res) => {
     await browser.close();
 
     console.log("PDF Generated");
+
+    // Send generated PDF
     res.sendFile(__dirname + "/certificate.pdf");
 });
 
